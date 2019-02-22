3c91f889abb3409bb4ecf4dc0de6628e87e1f4e6
David Bruno <davidbruno2000@gmail.com>
2019-02-21 21:47:43 -0500

    Uncle Charlie In Vegas Exam 2 Project

---
+++ cop-2800c/UncleCharlieIsInVegas/SlotMachine.java
@@ -0,0 +1,61 @@
+/*
+* File: SlotMachine.java
+* Name: David Bruno
+* Date: 2/21/19
+*/
+
+public class SlotMachine {
+    private String nameOfSlot;
+    private double balance;
+    private int numJackpots;
+    private int numWins;
+    private double payOut;
+
+    public SlotMachine(String nameOfSlot, double balance, int numJackpots, int numWins, double payOut){
+        setName(nameOfSlot);
+        setBalance(balance);
+        setJackpots(numJackpots);
+        setWins(numWins);
+        setPayOut(payOut);
+    }
+
+    public String getName(){
+        return nameOfSlot;
+    }
+
+    public double getBalance(){
+        return currBalance;
+    }
+
+    public int getNumJackpots(){
+        return numJackpots;
+    }
+
+    public int getNumWins(){
+        return numWins;
+    }
+
+    public double getPayOut(){
+        return payOut;
+    }
+
+    public void setName(String nameOfSlot){
+        this.nameOfSlot = nameOfSlot;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    public void setJackpots(int numJackpots){
+        this.numJackpots = numJackpots;
+    }
+
+    public void setWins(int numWins){
+        this.numWins = numWins;
+    }
+    
+    public void setPayOut(double payOut){
+        this.payOut = payOut;
+    }
+}
